3b4af02e9370056f618e102d6129ee006dbb8061
L3MON4D3 <Simljk@outlook.de>
2021-03-21 11:32:24 +0100

    Update Examples.

--- Examples/snippets.lua
+++ Examples/snippets.lua
@@ -1,9 +1,11 @@
 local ls = require'luasnip'
 -- some shorthands...
 local s = ls.s
+local sn = ls.sn
 local t = ls.t
 local i = ls.i
 local f = ls.f
+local c = ls.c
 
 -- args is a table, where 1 is the text in Placeholder 1, 2 the text in
 -- placeholder 2,...
@@ -28,5 +30,33 @@ ls.snippets = {
 		-- Last Placeholder, exit Point of the snippet. EVERY SNIPPET NEEDS Placeholder 0.
 		i(0),
 		t({"", "}"})
-	})
+	}),
+	s("class", {
+		-- Choice: Switch between two different Nodes, first parameter is its position, second a list of nodes.
+		c(1, {
+			t({"public "}),
+			t({"private "})
+		}),
+		t({"class "}),
+		i(2),
+		t({" "}),
+		c(3, {
+			t({"{"}),
+			-- sn: Nested Snippet. Instead of a trigger, it has a position, just like insert-nodes.
+			-- Inside Choices, Nodes don't need a position as the choice node is the one being jumped to.
+			sn(nil, {
+				t({"extends "}),
+				i(0),
+				t({" {"})
+			}),
+			sn(nil, {
+				t({"implements "}),
+				i(0),
+				t({" {"})
+			})
+		}),
+		t({"","\t"}),
+		i(0),
+		t({"", "}"})
+	}),
 }
