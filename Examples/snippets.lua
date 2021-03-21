78511ae431c0757abcab8c0683777f4f77f3a2b1
L3MON4D3 <Simljk@outlook.de>
2021-03-21 17:30:33 +0100

    Update Examples, add GIFs.

--- Examples/snippets.lua
+++ Examples/snippets.lua
@@ -15,18 +15,18 @@ ls.snippets = {
 	-- trigger is fn.
 	s("fn", {
 		-- Simple static text.
-		t({"function "}),
+		t({"//Parameters: "}),
+		-- function, first parameter is the function, second the Placeholders
+		-- whose text it gets as input.
+		f(copy, {2}),
+		t({"", "function "}),
 		-- Placeholder/Insert.
 		i(1),
 		t({"("}),
 		-- Placeholder with initial text.
 		i(2, {"int foo"}),
-		t({")"}),
 		-- Linebreak
-		t({" {","\t"}),
-		-- function, first parameter is the function, second the Placeholders
-		-- whose text it gets as input.
-		f(copy, {2}),
+		t({") {", "\t"}),
 		-- Last Placeholder, exit Point of the snippet. EVERY SNIPPET NEEDS Placeholder 0.
 		i(0),
 		t({"", "}"})
