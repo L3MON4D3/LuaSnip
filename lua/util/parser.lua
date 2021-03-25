5068196e24b697501402a67dc99c0842977dfbfb
L3MON4D3 <Simljk@outlook.de>
2021-03-25 21:42:07 +0100

    Parser, all WIP.

--- lua/util/parser.lua
+++ lua/util/parser.lua
@@ -3,11 +3,8 @@ local iNode = require 'nodes.insertNode'
 local fNode = require 'nodes.functionNode'
 local cNode = require 'nodes.choiceNode'
 local dNode = require 'nodes.dynamicNode'
-local snip = require 'nodes.snippet'
+local snipNode = require 'nodes.snippet'
 local functions = require 'util.functions'
-
-local tab_stops
-local brackets
 
 local function is_escaped(text, indx)
 	local count = 0
@@ -55,7 +52,7 @@ local function parse_text(text)
 	return tNode.T(text_table)
 end
 
-local function simple_tabstop(text)
+local function simple_tabstop(text, tab_stops)
 	local num = tonumber(text)
 	if not num then return nil end
 	if not tab_stops[num] then
@@ -68,13 +65,57 @@ local function simple_tabstop(text)
 	end
 end
 
+-- Needed for eg. nested Placeholders, internal snippets don't have the first
+-- num tabstops.
+local function decrease_tabstops(snip, num)
+	-- contains node with highest pos,
+	local highest = {pos = 0}
+	for i, node in ipairs(snip.nodes) do
+		if node.pos then
+			-- Remove automatically-inserted 0-ins. IF there is no other insert.
+			if node.pos ~= 0 then
+				node.pos = node.pos - num
+			end
+
+			if node.pos > highest.pos then
+				highest = node
+			end
+		end
+	end
+	if highest.pos ~= 0 then
+		highest.pos = 0
+	end
+end
+
+local function brackets_offset(list, offset)
+	local l_new = {}
+	for k,v in pairs(list) do
+		l_new[k+offset] = v + offset
+	end
+	return l_new
+end
+
+local parse_snippet
+
+function parse_placeholder(text, tab_stops, brackets)
+	local start, stop, match = string.find(text, "(%d+):")
+	if start == 1 then
+		local pos = tonumber(match)
+		local snip = parse_snippet(nil, string.sub(text, stop+1, #text), tab_stops, brackets_offset(brackets, -stop))
+		if snip then
+			decrease_tabstops(snip, pos)
+			tab_stops[pos] = cNode.C(pos, {snip, tNode.T({""})})
+			return tab_stops[pos]
+		end
+	end
+	return nil
+end
+
 local parse_functions={simple_tabstop, parse_placeholder, parse_choice, parse_variable, error}
 
-local function parse_snippet(trigger, body)
-	-- reset state.
-	tab_stops = {}
-	brackets = {}
-	brackets = brckt_lst(body)
+parse_snippet = function(trigger, body, tab_stops, brackets)
+	if not brackets then brackets = brckt_lst(body) end
+	if not tab_stops then tab_stops = {} end
 
 	local nodes = {}
 	local indx = 1
@@ -98,7 +139,7 @@ local function parse_snippet(trigger, body)
 					local nodestring = string.sub(body, next_node+2, match_bracket-1)
 					local node
 					for _, fn in ipairs(parse_functions) do
-						node = fn(nodestring)
+						node = fn(nodestring, tab_stops, brackets_offset(brackets, -(next_node+1)))
 						if node then break end
 					end
 					nodes[#nodes+1] = node
@@ -107,12 +148,12 @@ local function parse_snippet(trigger, body)
 				elseif string.find(body, "%d", next_node+1) == next_node+1 then
 					local _, last_char, match = string.find(body, "(%d+)", next_node+1)
 					-- Add insert- or copy-function-node.
-					nodes[#nodes+1] = simple_tabstop(match)
+					nodes[#nodes+1] = simple_tabstop(match, tab_stops)
 					indx = last_char+1
 				elseif string.find(body, "%w", next_node+1) == next_node+1 then
 					local _, last_char, match = string.find(body, "(%w+)", next_node+1)
 					-- Add var-node
-					nodes[#nodes+1] = simple_var(match)
+					nodes[#nodes+1] = simple_var(match, tab_stops)
 					indx = last_char+1
 				else
 					error("Invalid text after $ at"..tostring(next_node+1))
@@ -133,7 +174,7 @@ local function parse_snippet(trigger, body)
 			if not tab_stops[0] then
 				nodes[#nodes+1] = iNode.I(0)
 			end
-			return snip.S(trigger, nodes)
+			return snipNode.S(trigger, nodes)
 		end
 	end
 end
