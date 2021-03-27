721e0bb399249d64f7b715d92171efd10753ca3e
L3MON4D3 <Simljk@outlook.de>
2021-03-27 17:46:02 +0100

    Correct behaviour of ZeroNode:input_enter.

--- lua/nodes/insertNode.lua
+++ lua/nodes/insertNode.lua
@@ -12,7 +12,6 @@ end
 
 function ZeroNode:jump_into(dir)
 	self:input_enter()
-	self:input_leave()
 	if dir == 1 then
 		if self.next then
 			self.next:jump_into(dir)
@@ -29,6 +28,19 @@ function ZeroNode:jump_into(dir)
 		end
 	end
 	return true
+end
+
+function ZeroNode:input_enter()
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), 'n', true)
+	-- SELECT snippet text only when there is text to select (more oft than not there isnt).
+	if not util.mark_pos_equal(self.markers[2], self.markers[1]) then
+		util.normal_move_on_mark(self.markers[1])
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("v", true, false, true), 'n', true)
+		util.normal_move_before_mark(self.markers[2])
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("o<C-G>", true, false, true), 'n', true)
+	else
+		util.normal_move_on_mark_insert(self.markers[1])
+	end
 end
 
 function InsertNode:input_enter()
