b51679d1b3fc5e2c602edd9927ebdeb1bc83cc75
leiserfg <leiserfg@gmail.com>
2021-04-09 18:56:34 +0200

    Add example

--- Examples/snippets.lua
+++ Examples/snippets.lua
@@ -191,3 +191,12 @@ ls.snippets = {
 	}
 }
 
+--[[
+-- Beside defining your own snippets you can also load snippets from "vscode-like" packages defining
+-- that expose snippets in json files, for example <https://github.com/rafamadriz/friendly-snippets>.
+-- Mind that this will extend  `ls.snippets` so you need to do it after your own snippets or you
+-- will need to extend the table yourself instead of setting a new one.
+]]
+
+require('luasnip/loaders/from_vscode').load()
+
