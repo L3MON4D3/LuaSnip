9be1002b1b63127484affec8d05c0a6c93306089
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-06-25 01:08:17 +0200

    Use verbose function names in examples

--- Examples/snippets.lua
+++ Examples/snippets.lua
@@ -1,12 +1,12 @@
 local ls = require("luasnip")
 -- some shorthands...
-local s = ls.s
-local sn = ls.sn
-local t = ls.t
-local i = ls.i
-local f = ls.f
-local c = ls.c
-local d = ls.d
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
 
 -- Every unspecified option will be set to the default.
 ls.config.set_config({
