9296397fd254b7ea2d506e7d470d4188cb6b7e8c
L3MON4D3 <Simljk@outlook.de>
2021-08-01 21:40:32 +0200

    Reset Visual after use.

--- lua/luasnip/util/util.lua
+++ lua/luasnip/util/util.lua
@@ -180,6 +180,7 @@ local LUASNIP_LAST_SELECTION = "LUASNIP_LAST_SELECTION"
 
 local function get_selection()
 	local ok, val = pcall(vim.api.nvim_buf_get_var, 0, LUASNIP_LAST_SELECTION)
+	vim.api.nvim_buf_set_var(0, LUASNIP_LAST_SELECTION, {})
 	return ok and val or {}
 end
 
