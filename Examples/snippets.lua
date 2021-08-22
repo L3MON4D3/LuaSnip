cb27f3f64ebea15cef3bef3f2986448fc3fdc2f0
leiserfg <leiserfg@gmail.com>
2021-08-22 10:46:58 +0200

    Clean lazy example and add missing local

--- Examples/snippets.lua
+++ Examples/snippets.lua
@@ -401,4 +401,4 @@ require("luasnip/loaders/from_vscode").load({ include = { "python" } }) -- Load 
 require("luasnip/loaders/from_vscode").load({ paths = { "./my-snippets" } }) -- Load snippets from my-snippets folder
 
 -- You can also use lazy loading so you only get in memory snippets of languages you use
-require("luasnip/loaders/from_vscode").lazy_load() -- You can pass { path = "./my-snippets/"} as well
+require("luasnip/loaders/from_vscode").lazy_load() -- You can pass { paths = "./my-snippets/"} as well
