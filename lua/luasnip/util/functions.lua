ca1cf57f1368871479a97435a53c64dc3cc19175
leiserfg <leiserfg@gmail.com>
2021-08-29 09:15:45 +0200

    Handle empty vars

--- lua/luasnip/util/functions.lua
+++ lua/luasnip/util/functions.lua
@@ -2,7 +2,12 @@ return {
 	var = function(_, node, text)
 		local v = node.parent.env[text]
 		if type(v) == "table" then
-			return v
+			-- Avoid issues with empty vars
+			if #v == 0 then
+				return { "" }
+			else
+				return {}
+			end
 		else
 			return { v }
 		end
