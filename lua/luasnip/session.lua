64f3ce04967fe7d3dd4cc59eb61f4bfdd2b0beed
L3MON4D3 <Simljk@outlook.de>
2021-09-11 11:25:08 +0200

    fix: return and set same value in __index.
    Otherwise, tbl_extend will extend the returned value and not have any
    effect on the value in the table.

--- lua/luasnip/session.lua
+++ lua/luasnip/session.lua
@@ -6,9 +6,10 @@ setmetatable(M.ft_redirect, {
 	__index = function(table, key)
 		-- no entry for this ft(key), set it to avoid calls on each expand for
 		-- this filetype.
-		rawset(table, key, { key })
-		return { key }
-	end,
+		local val = {key}
+		rawset(table, key, val)
+		return val
+	end
 })
 
 return M
