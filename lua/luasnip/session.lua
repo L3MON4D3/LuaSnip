8fbb72a04c9c916d22d753eade2aab5fb67c88ef
L3MON4D3 <L3MON4D3@users.noreply.github.com>
2021-09-11 21:12:04 +0000

    Format with stylua

--- lua/luasnip/session.lua
+++ lua/luasnip/session.lua
@@ -6,10 +6,10 @@ setmetatable(M.ft_redirect, {
 	__index = function(table, key)
 		-- no entry for this ft(key), set it to avoid calls on each expand for
 		-- this filetype.
-		local val = {key}
+		local val = { key }
 		rawset(table, key, val)
 		return val
-	end
+	end,
 })
 
 return M
