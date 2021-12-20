3e4da0cfea0a2f0b4749369bc7ed247c1412a854
L3MON4D3 <L3MON4D3@users.noreply.github.com>
2021-12-20 17:22:13 +0000

    Format with stylua

--- lua/luasnip/extras/init.lua
+++ lua/luasnip/extras/init.lua
@@ -140,7 +140,9 @@ return {
 		local insert_preset_text_func = lambda.instantiate(lambd)
 		return D(pos, function(args, imm_parent)
 			-- to be sure, lambda may end with a `match` returning nil.
-			local out = insert_preset_text_func(make_lambda_args(args, imm_parent)) or ""
+			local out = insert_preset_text_func(
+				make_lambda_args(args, imm_parent)
+			) or ""
 			return SN(pos, {
 				I(1, vim.split(out, "\n")),
 			})
