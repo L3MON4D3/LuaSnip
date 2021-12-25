85cce598b24361eeae6d49f49a8b6f51d7e3a14b
L3MON4D3 <Simljk@outlook.de>
2021-12-25 19:58:32 +0100

    tests: adapt tests to use nvims' testing-framework.

--- tests/fmt_spec.lua
+++ tests/fmt_spec.lua
@@ -1,8 +1,14 @@
-local interpolate = require("luasnip.extras.fmt").interpolate
+local helpers = require("test.functional.helpers")(after_each)
 
 local works = function(msg, fmt, args, expected, opts)
 	it(msg, function()
-		local result = interpolate(fmt, args, opts)
+		-- normally `exec_lua` accepts a table which is passed to the code as `...`, but it
+		-- fails if number- and string-keys are mixed ({1,2} works fine, {1, b=2} fails).
+		-- So we limit ourselves to just passing strings, which are then turned into tables
+		-- while load()ing the function.
+		local result = helpers.exec_lua(string.format(
+			"return require(\"luasnip.extras.fmt\").interpolate(\"%s\", %s, %s)",
+			fmt, args, opts))
 		assert.are.same(expected, table.concat(result))
 	end)
 end
@@ -10,80 +16,87 @@ end
 local fails = function(msg, fmt, args, opts)
 	it(msg, function()
 		assert.has_error(function()
-			interpolate(fmt, args, opts)
+			helpers.exec_lua(string.format(
+				"return require(\"luasnip.extras.fmt\").interpolate(\"%s\", %s, %s)",
+				fmt, args, opts))
 		end)
 	end)
 end
 
 describe("fmt.interpolate", function()
-	works("expands with no numbers", "a{}b{}c{}d", { 4, 5, 6 }, "a4b5c6d")
+	-- apparently clear() needs to run before anything else...
+	helpers.clear()
+	-- set in makefile.
+	helpers.exec("set rtp+="..os.getenv("LUASNIP_SOURCE"))
+
+	works("expands with no numbers", "a{}b{}c{}d", "{ 4, 5, 6 }", "a4b5c6d")
 
 	works(
 		"expands with explicit numbers",
 		"a{2}b{1}c{3}d",
-		{ 4, 5, 6 },
+		"{ 4, 5, 6 }",
 		"a5b4c6d"
 	)
 
 	works(
 		"expands with mixed numbering",
 		"a{}b{3}c{}d{2}e",
-		{ 1, 2, 3, 4 },
+		"{ 1, 2, 3, 4 }",
 		"a1b3c4d2e"
 	)
 
 	works(
 		"expands named placeholders",
 		"a{A}b{B}c{C}d",
-		{ A = 1, B = 2, C = 3 },
+		"{ A = 1, B = 2, C = 3 }",
 		"a1b2c3d"
 	)
 
 	works(
 		"expands all mixed",
 		"a {A} b {} c {3} d {} e {B} f {A} g {2} h",
-		{ 1, 2, 3, 4, A = 10, B = 20 },
+		"{ 1, 2, 3, 4, A = 10, B = 20 }",
 		"a 10 b 1 c 3 d 4 e 20 f 10 g 2 h"
 	)
 
 	works(
 		"current index changed by numbered nodes",
 		"{} {} {1} {} {}",
-		{ 1, 2, 3 },
+		"{ 1, 2, 3 }",
 		"1 2 1 2 3"
 	)
 
-	works("excludes trailing text", "{}abcd{}", { 1, 2 }, "1abcd2")
+	works("excludes trailing text", "{}abcd{}", "{ 1, 2 }", "1abcd2")
 
 	works(
 		"escapes empty double-braces",
 		"a{{}}b{}c{{}}d{}e",
-		{ 2, 4 },
+		"{ 2, 4 }",
 		"a{}b2c{}d4e"
 	)
 
-	works("escapes non-empty double-braces", "a{{d}}b{}c", { 2 }, "a{d}b2c")
+	works("escapes non-empty double-braces", "a{{d}}b{}c", "{ 2 }", "a{d}b2c")
 
 	works(
 		"do not trim placeholders with whitespace",
 		"a{ something}b{}c",
-		{ 2, [" something"] = 1 },
+		"{ 2, [\" something\"] = 1 }",
 		"a1b2c"
 	)
 
 	works(
 		"replaces nested escaped braces",
 		"a{{{{}}}}b{}c{{ {{ }}}}d",
-		{ 2 },
+		"{ 2 }",
 		"a{{}}b2c{ { }}d"
 	)
 
-	works("replaces umatched escaped braces", "a{{{{b{}c", { 2 }, "a{{b2c")
+	works("replaces umatched escaped braces", "a{{{{b{}c", "{ 2 }", "a{{b2c")
 
 	works(
 		"replaces in braces inside escaped braces",
 		"a{{{}}}b{{ {}}}c{{{} }}d{{ {} }}e",
-		{ 1, 2, 3, 4 },
+		"{ 1, 2, 3, 4 }",
 		"a{1}b{ 2}c{3 }d{ 4 }e"
 	)
 
@@ -94,9 +107,9 @@ describe("fmt.interpolate", function()
 	works(
 		"can use different delimiters",
 		"foo() { return <>; };",
-		{ 10 },
+		"{ 10 }",
 		"foo() { return 10; };",
-		{ delimiters = "<>" }
+		"{ delimiters = \"<>\" }"
 	)
 
 	local delimiters = { "()", "[]", "<>", "%$", "#@", "?!" }
@@ -106,9 +119,9 @@ describe("fmt.interpolate", function()
 			works(
 				delims,
 				string.format("{ return %s%s; };", left, right),
-				{ 10 },
+				"{ 10 }",
 				"{ return 10; };",
-				{ delimiters = delims }
+				string.format("{ delimiters = \"%s\" }", delims)
 			)
 		end)
 	end
@@ -116,32 +129,32 @@ describe("fmt.interpolate", function()
 	works(
 		"can escape custom delimiters",
 		"foo((x)) { return x + (); };",
-		{ 10 },
+		"{ 10 }",
 		"foo(x) { return x + 10; };",
-		{ delimiters = "()" }
+		"{ delimiters = \"()\" }"
 	)
 
 	works(
 		"can use named placeholders with custom delimiters",
 		"foo(x) { return x + [y]; };",
-		{ y = 10 },
+		"{ y = 10 }",
 		"foo(x) { return x + 10; };",
-		{ delimiters = "[]" }
+		"{ delimiters = \"[]\" }"
 	)
 
-	fails("dissallows unused list args", "a {} b {} c", { 1, 2, 3 })
+	fails("dissallows unused list args", "a {} b {} c", "{ 1, 2, 3 }")
 
 	fails(
 		"dissallows unused map args",
 		"a {A} b {B} c {} d",
-		{ 1, A = 10, B = 20, C = 30 }
+		"{ 1, A = 10, B = 20, C = 30 }"
 	)
 
 	works(
 		"allows unused with strict=false",
 		"a {A} b {B} c {} d",
-		{ 1, 2, A = 10, B = 20, C = 30 },
+		"{ 1, 2, A = 10, B = 20, C = 30 }",
 		"a 10 b 20 c 1 d",
-		{ strict = false }
+		"{ strict = false }"
 	)
 end)
