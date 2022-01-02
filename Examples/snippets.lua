b5a72f1fbde545be101fcd10b70bcd51ea4367de
L3MON4D3 <L3MON4D3@users.noreply.github.com>
2022-01-02 08:59:21 +0000

    Format with stylua

--- Examples/snippets.lua
+++ Examples/snippets.lua
@@ -491,7 +491,6 @@ ls.filetype_extend("lua", { "c" })
 -- in a cpp file: search c-snippets, then all-snippets only (no cpp-snippets!!).
 ls.filetype_set("cpp", { "c" })
 
-
 --[[
 -- Beside defining your own snippets you can also load snippets from "vscode-like" packages
 -- that expose snippets in json files, for example <https://github.com/rafamadriz/friendly-snippets>.
@@ -522,4 +521,3 @@ require("luasnip.loaders.from_snipmate").load({ path = { "./my-snippets" } }) --
 -- `~/.config/nvim/snippets`).
 
 require("luasnip.loaders.from_snipmate").lazy_load() -- Lazy loading
-
