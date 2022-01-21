2ebfa490b8a2a84dc9a58ee2622209c3cec582a8
L3MON4D3 <Simljk@outlook.de>
2022-01-21 18:14:04 +0100

    Respect different newline conventions.
    DOS: lines are _separated_ by \r\n.
    Unix: lines are _terminated_ with \n.
    Mac (pre-OS X): lines are (assumption!) _terminated_ with \r.
    
    The previous implementation only considered Unix' convention, leading to
    trailing \r on files created with the DOS convention.

--- lua/luasnip/loaders/util.lua
+++ lua/luasnip/loaders/util.lua
@@ -15,6 +15,33 @@ local function filetypelist_to_set(list)
 	return out
 end
 
+local function split_lines(filestring)
+	-- test for used line separator and split accordingly.
+	if filestring:find("\r\n") then
+		return vim.split(
+			filestring,
+			"\r\n",
+			{ plain = true, trimemtpy = false }
+		)
+	elseif filestring:find("\r") then
+		-- both mac and unix-files contain a trailing newline which would lead
+		-- to an additional empty line being read (\r, \n _terminate_ lines, they
+		-- don't _separate_ them)
+		return vim.split(
+			filestring:sub(1, #filestring - 1),
+			"\r",
+			{ plain = true, trimemtpy = false }
+		)
+	else
+		return vim.split(
+			filestring:sub(1, #filestring - 1),
+			"\n",
+			{ plain = true, trimemtpy = false }
+		)
+	end
+end
+
 return {
 	filetypelist_to_set = filetypelist_to_set,
+	split_lines = split_lines,
 }
---
+++ tests/unit/split_lines_spec.lua
@@ -0,0 +1,61 @@
+local helpers = require("test.functional.helpers")(after_each)
+local exec_lua = helpers.exec_lua
+
+describe("split_lines", function()
+	local function check(test_name, filestring, lines)
+		-- escape control-characters.
+		filestring = filestring:gsub("\r", "\\r")
+		filestring = filestring:gsub("\n", "\\n")
+		it(test_name, function()
+			assert.are.same(
+				lines,
+				exec_lua(
+					'return require("luasnip.loaders.util").split_lines("'
+						.. filestring
+						.. '")'
+				)
+			)
+		end)
+	end
+
+	-- apparently clear() needs to run before anything else...
+	helpers.clear()
+	-- LUASNIP_SOURCE is set in makefile (or before that, even).
+	helpers.exec("set rtp+=" .. os.getenv("LUASNIP_SOURCE"))
+
+	check("works for DOS-files", "aaa\r\nbbb\r\nccc", { "aaa", "bbb", "ccc" })
+	check(
+		"works for DOS-files with empty last line",
+		"aaa\r\nbbb\r\nccc\r\n",
+		{ "aaa", "bbb", "ccc", "" }
+	)
+	check(
+		"works for DOS-files with multiple empty lines",
+		"aaa\r\n\r\n\r\nccc",
+		{ "aaa", "", "", "ccc" }
+	)
+
+	check("works for unix-files", "aaa\nbbb\nccc\n", { "aaa", "bbb", "ccc" })
+	check(
+		"works for unix-files with empty last line",
+		"aaa\nbbb\nccc\n\n",
+		{ "aaa", "bbb", "ccc", "" }
+	)
+	check(
+		"works for unix-files with multiple empty lines",
+		"aaa\n\n\nccc\n",
+		{ "aaa", "", "", "ccc" }
+	)
+
+	check("works for mac-files", "aaa\rbbb\rccc\r", { "aaa", "bbb", "ccc" })
+	check(
+		"works for mac-files with empty last line",
+		"aaa\rbbb\rccc\r\r",
+		{ "aaa", "bbb", "ccc", "" }
+	)
+	check(
+		"works for mac-files with multiple empty lines",
+		"aaa\r\r\rccc\r",
+		{ "aaa", "", "", "ccc" }
+	)
+end)
