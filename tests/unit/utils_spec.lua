40f5b5e965c162efcd1575b0a40307a01d304951
leiserfg <leiserfg@users.noreply.github.com>
2022-02-13 09:02:01 +0000

    Format with stylua

--- tests/unit/utils_spec.lua
+++ tests/unit/utils_spec.lua
@@ -1,7 +1,6 @@
 local helpers = require("test.functional.helpers")(after_each)
 local exec_lua = helpers.exec_lua
 
-
 describe("luasnip.util.str:dedent", function()
 	local function check(test_name, input, output)
 		it(test_name, function()
@@ -10,14 +9,14 @@ describe("luasnip.util.str:dedent", function()
 				exec_lua(
 					'return require("luasnip.util.str").dedent([['
 						.. input
-						.. ']])'
+						.. "]])"
 				)
 			)
 		end)
 	end
 
-    check("2 and 0", "   one", "one")
-    check("0 and 2", "one\n  two", "one\n  two")
-    check("2 and 1", "  one\n two", " one\ntwo")
-    check("2 and 2", "  one\n  two", "one\ntwo")
-end )
+	check("2 and 0", "   one", "one")
+	check("0 and 2", "one\n  two", "one\n  two")
+	check("2 and 1", "  one\n two", " one\ntwo")
+	check("2 and 2", "  one\n  two", "one\ntwo")
+end)
