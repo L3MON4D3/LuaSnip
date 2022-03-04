20808ecce852bd2cdb973b99df324fcfd4e60cea
L3MON4D3 <Simljk@outlook.de>
2022-03-04 10:35:21 +0100

    Allow using vim.ui.select for selecting a choice (closes #308).

---
+++ lua/luasnip/extras/select_choice.lua
@@ -0,0 +1,33 @@
+local session = require("luasnip.session")
+local util = require("luasnip.util.util")
+
+local function set_choice_callback(_, indx)
+	local choice = indx and session.active_choice_node.choices[indx]
+	if not choice then
+		return
+	end
+	local new_active = util.no_region_check_wrap(
+		session.active_choice_node.set_choice,
+		session.active_choice_node,
+		choice,
+		session.current_nodes[vim.api.nvim_get_current_buf()]
+	)
+	session.current_nodes[vim.api.nvim_get_current_buf()] = new_active
+end
+
+local function active_choice_get_choices_text()
+	local choice_lines = {}
+
+	for i, choice in ipairs(session.active_choice_node.choices) do
+		choice_lines[i] = table.concat(choice:get_docstring(), "\n")
+	end
+
+	return choice_lines
+end
+
+local function select_choice()
+	assert(session.active_choice_node, "No active choiceNode")
+	vim.ui.select(active_choice_get_choices_text(), {}, set_choice_callback)
+end
+
+return select_choice
