2c65f5d7e492d4bc4b7d7c8a4aa0c503136abd97
Yilin Wei <yilin@kebab-ca.se>
2022-03-16 15:08:29 +0000

    Adding initial treesitter expansion conditions.
    Add `expand_conditions.node_types` to expand
    snippets within specific treesitter nodes.

--- lua/luasnip/extras/expand_conditions.lua
+++ lua/luasnip/extras/expand_conditions.lua
@@ -5,4 +5,24 @@ function M.line_begin(line_to_cursor, matched_trigger)
 	return line_to_cursor:sub(1, -(#matched_trigger + 1)):match("^%s*$")
 end
 
+function M.node_types(node_types)
+
+	local ts_utils = require'nvim-treesitter.ts_utils'
+	local node_types = type(node_types) == "table" and node_types or { node_types }
+
+	local function cond()
+		local node = ts_utils.get_node_at_cursor()
+                if node ~= nil then
+		        for _, node_type in ipairs(node_types) do
+			        if node_type == node:type() then
+				        return true
+			        end
+		        end
+                end
+		return false
+	end
+
+	return cond
+end
+
 return M
