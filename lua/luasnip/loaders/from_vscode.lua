31fe9d9bbbc2470767e09d3c71c76c9b12b18ed7
L3MON4D3 <Simljk@outlook.de>
2022-05-04 23:11:06 +0200

    restructure vscode-loader (not done yet).
    The design will be more similar to the lua/snipmate-loader.

--- lua/luasnip/loaders/from_vscode.lua
+++ lua/luasnip/loaders/from_vscode.lua
@@ -14,7 +14,7 @@ local function json_decode(data)
 	end
 end
 
-local function load_snippet_file(langs, snippet_set_path)
+local function load_snippet_file(lang, snippet_set_path)
 	if not Path.exists(snippet_set_path) then
 		return
 	end
@@ -24,57 +24,43 @@ local function load_snippet_file(langs, snippet_set_path)
 		return
 	end
 
-	for _, lang in pairs(langs) do
-		local lang_snips = {}
-		local auto_lang_snips = {}
-		for name, parts in pairs(snippet_set_data) do
-			local body = type(parts.body) == "string" and parts.body
-				or table.concat(parts.body, "\n")
+	-- TODO: make check if file was already parsed once, we can store+reuse the
+	-- snippets.
 
-			-- There are still some snippets that fail while loading
-			pcall(function()
-				-- Sometimes it's a list of prefixes instead of a single one
-				local prefixes = type(parts.prefix) == "table" and parts.prefix
-					or { parts.prefix }
-				for _, prefix in ipairs(prefixes) do
-					local ls_conf = parts.luasnip or {}
+	local lang_snips = {}
+	local auto_lang_snips = {}
+	for name, parts in pairs(snippet_set_data) do
+		local body = type(parts.body) == "string" and parts.body
+			or table.concat(parts.body, "\n")
 
-					local snip = sp({
-						trig = prefix,
-						name = name,
-						dscr = parts.description or name,
-						wordTrig = true,
-					}, body)
+		-- There are still some snippets that fail while loading
+		pcall(function()
+			-- Sometimes it's a list of prefixes instead of a single one
+			local prefixes = type(parts.prefix) == "table" and parts.prefix
+				or { parts.prefix }
+			for _, prefix in ipairs(prefixes) do
+				local ls_conf = parts.luasnip or {}
 
-					if ls_conf.autotrigger then
-						table.insert(auto_lang_snips, snip)
-					else
-						table.insert(lang_snips, snip)
-					end
+				local snip = sp({
+					trig = prefix,
+					name = name,
+					dscr = parts.description or name,
+					wordTrig = true,
+				}, body)
+
+				if ls_conf.autotrigger then
+					table.insert(auto_lang_snips, snip)
+				else
+					table.insert(lang_snips, snip)
 				end
-			end)
-		end
-		ls.add_snippets(lang, lang_snips, { type = "snippets" })
-		ls.add_snippets(lang, auto_lang_snips, { type = "autosnippets" })
+			end
+		end)
 	end
+	ls.add_snippets(lang, lang_snips, { type = "snippets" })
+	ls.add_snippets(lang, auto_lang_snips, { type = "autosnippets" })
 end
 
-local function filter_list(list, exclude, include)
-	local out = {}
-	for _, entry in ipairs(list) do
-		if exclude[entry] then
-			goto continue
-		end
-		-- If include is nil then it's true
-		if include == nil or include[entry] then
-			table.insert(out, entry)
-		end
-		::continue::
-	end
-	return out
-end
-
-local function load_snippet_folder(root, opts)
+local function package_files(root, filter)
 	local package = Path.join(root, "package.json")
 	local data = Path.read_file(package)
 	local package_data = json_decode(data)
@@ -85,21 +71,30 @@ local function load_snippet_folder(root, opts)
 			and package_data.contributes.snippets
 		)
 	then
-		return
+		-- root doesn't contain a package.json, return no snippets.
+		return {}
 	end
+
+	-- stores ft -> files(string[]).
+	local ft_files = {}
 
 	for _, snippet_entry in pairs(package_data.contributes.snippets) do
 		local langs = snippet_entry.language
 
-		if type(snippet_entry.language) ~= "table" then
+		if type(langs) ~= "table" then
 			langs = { langs }
 		end
-		langs = filter_list(langs, opts.exclude, opts.include)
-
-		if #langs ~= 0 then
-			load_snippet_file(langs, Path.join(root, snippet_entry.path))
+		for _, ft in ipairs(langs) do
+			if filter(ft) then
+				if not ft_files[ft] then
+					ft_files[ft] = {}
+				end
+				table.insert(ft_files[ft], Path.join(root, snippet_entry.path))
+			end
 		end
 	end
+
+	return ft_files
 end
 
 local function get_snippet_rtp()
@@ -108,27 +103,41 @@ local function get_snippet_rtp()
 	end, vim.api.nvim_get_runtime_file("package.json", true))
 end
 
-local M = {}
-function M.load(opts)
+-- sanitizes opts and returns ft -> files-map for `opts` (respects in/exclude).
+local function get_snippet_files(opts)
 	opts = opts or {}
-	-- nil (unset) to include all languages (default), a list for the ones you wanna include
-	opts.include = loader_util.filetypelist_to_set(opts.include)
 
-	-- A list for the ones you wanna exclude (empty by default)
-	opts.exclude = loader_util.filetypelist_to_set(opts.exclude) or {}
-
+	local paths
 	-- list of paths to crawl for loading (could be a table or a comma-separated-list)
 	if not opts.paths then
-		opts.paths = get_snippet_rtp()
+		paths = get_snippet_rtp()
 	elseif type(opts.paths) == "string" then
-		opts.paths = vim.split(opts.paths, ",")
+		paths = vim.split(opts.paths, ",")
+	else
+		paths = opts.paths
+	end
+	paths = vim.tbl_map(Path.expand, paths) -- Expand before deduping, fake paths will become nil
+	paths = util.deduplicate(paths) -- Remove doppelgänger paths and ditch nil ones
+
+	local ft_paths = {}
+
+	local ft_filter = loader_util.ft_filter(opts.exclude, opts.include)
+	for _, root_path in ipairs(paths) do
+		loader_util.extend_ft_paths(
+			ft_paths,
+			package_files(root_path, ft_filter)
+		)
 	end
 
-	opts.paths = vim.tbl_map(Path.expand, opts.paths) -- Expand before deduping, fake paths will become nil
-	opts.paths = util.deduplicate(opts.paths) -- Remove doppelgänger paths and ditch nil ones
+	return ft_paths
+end
 
-	for _, path in ipairs(opts.paths) do
-		load_snippet_folder(path, opts)
+local M = {}
+function M.load(opts)
+	for ft, files in pairs(get_snippet_files(opts)) do
+		for _, file in ipairs(files) do
+			load_snippet_file(ft, file)
+		end
 	end
 end
 
