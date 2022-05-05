7b781349857ffefd7c584ba08396f558f649cd08
L3MON4D3 <Simljk@outlook.de>
2022-05-05 18:37:12 +0200

    Prepare snipmate-loader for reload-capability.

--- lua/luasnip/loaders/from_snipmate.lua
+++ lua/luasnip/loaders/from_snipmate.lua
@@ -74,61 +74,68 @@ local function parse_snipmate(buffer, filename)
 	return snippets.snippet, snippets.autosnippet, extends
 end
 
-local function load_snippet_file(path)
-	if not Path.exists(path) then
-		return
+local function load_snippet_files(add_ft, paths, collection_files, add_opts)
+	for _, path in ipairs(paths) do
+		if not Path.exists(path) then
+			return
+		end
+
+		local snippet, autosnippet, extends
+
+		if cache.path_snippets[path] then
+			snippet = cache.path_snippets[path].snippet
+			autosnippet = cache.path_snippets[path].autosnippet
+			extends = cache.path_snippets[path].extends
+		else
+			local buffer = Path.read_file(path)
+			snippet, autosnippet, extends = parse_snipmate(buffer, path)
+			cache.path_snippets[path] = {
+				snippet = snippet,
+				autosnippet = autosnippet,
+				extends = extends,
+			}
+		end
+
+		ls.add_snippets(
+			add_ft,
+			snippet,
+			vim.tbl_extend("keep", {
+				type = "snippets",
+				-- key has to include the filetype since one file may be loaded in
+				-- multiple filetypes (via `extends`).
+				key = string.format("__%s_snippets_%s", add_ft, path),
+			}, add_opts)
+		)
+		ls.add_snippets(
+			add_ft,
+			autosnippet,
+			vim.tbl_extend("keep", {
+				type = "autosnippets",
+				key = string.format("__%s_autosnippets_%s", add_ft, path),
+			}, add_opts)
+		)
+
+		for _, ft in ipairs(extends) do
+			-- "or {}" because the ft might (if the extended filetype is not
+			-- actually present in the collection) be nil.
+			load_snippet_files(
+				add_ft,
+				collection_files[ft] or {},
+				collection_files,
+				add_opts
+			)
+		end
 	end
-
-	local snippet, autosnippet, extends
-
-	if cache.path_snippets[path] then
-		snippet = cache.path_snippets[path].snippet
-		autosnippet = cache.path_snippets[path].autosnippet
-		extends = cache.path_snippets[path].extends
-	else
-		local buffer = Path.read_file(path)
-		snippet, autosnippet, extends = parse_snipmate(buffer)
-		cache.path_snippets[path] = {
-			snippet = snippet,
-			autosnippet = autosnippet,
-			extends = extends,
-		}
-	end
-
-	return snippet, autosnippet, extends
 end
 
 local M = {}
 
-function M._load(ft, collection_files)
-	local snippets = {}
-	local autosnippets = {}
-	-- _load might be called for non-existing filetype via `extends`-directive,
-	-- protect against that via `or {}` (we fail silently, though, maybe we
-	-- should throw an error/print some message).
-	for _, path in ipairs(collection_files[ft] or {}) do
-		local file_snippets, file_autosnippets, extends = load_snippet_file(
-			path
-		)
-		vim.list_extend(snippets, file_snippets)
-		vim.list_extend(autosnippets, file_autosnippets)
-		for _, extend in ipairs(extends) do
-			local extend_snippets, extend_autosnippets = M._load(
-				extend,
-				collection_files
-			)
-			vim.list_extend(snippets, extend_snippets)
-			vim.list_extend(autosnippets, extend_autosnippets)
-		end
-	end
-	return snippets, autosnippets
-end
-
 function M.load(opts)
 	opts = opts or {}
 	local add_opts = opts.add_opts or {}
+
 	-- we need all paths available in the collection for `extends`.
-	-- load_paths alone is influenced by in/exclude.
+	-- only load_paths is influenced by in/exclude.
 	local load_paths, collection_paths =
 		loader_util.get_load_paths_snipmate_like(
 			opts,
@@ -136,22 +143,12 @@ function M.load(opts)
 			"snippets"
 		)
 
-	-- also add files from collection to cache (collection of all loaded
+	-- also add files from load_paths to cache (collection of all loaded
 	-- files by filetype, useful for editing files for some filetype).
 	loader_util.extend_ft_paths(cache.ft_paths, load_paths)
 
-	for ft, _ in pairs(load_paths) do
-		local snippets, autosnippets = M._load(ft, collection_paths)
-		ls.add_snippets(
-			ft,
-			snippets,
-			vim.tbl_extend("keep", { type = "snippets" }, add_opts)
-		)
-		ls.add_snippets(
-			ft,
-			autosnippets,
-			vim.tbl_extend("keep", { type = "autosnippets" }, add_opts)
-		)
+	for ft, paths in pairs(load_paths) do
+		load_snippet_files(ft, paths, collection_paths, add_opts)
 	end
 end
 
@@ -163,12 +160,12 @@ function M._lazyload()
 			for _, collection_load_paths in ipairs(cache.lazy_load_paths) do
 				-- don't load if this ft wasn't included/was excluded.
 				if collection_load_paths[ft] then
-					local snippets, autosnippets = M._load(
+					load_snippet_files(
 						ft,
-						collection_load_paths.collection
+						collection_load_paths[ft],
+						collection_load_paths.collection,
+						collection_load_paths.add_opts
 					)
-					ls.add_snippets(ft, snippets, { type = "snippets" })
-					ls.add_snippets(ft, autosnippets, { type = "autosnippets" })
 				end
 			end
 			cache.lazy_loaded_ft[ft] = true
@@ -189,22 +186,10 @@ function M.lazy_load(opts)
 
 	loader_util.extend_ft_paths(cache.ft_paths, load_paths)
 
-	for ft, _ in pairs(load_paths) do
+	for ft, paths in pairs(load_paths) do
 		if cache.lazy_loaded_ft[ft] then
-			-- instantly load snippets if they were already loaded...
-			local snippets, autosnippets = M._load(ft, collection_paths)
-			Insp(vim.tbl_extend("keep", { type = "snippets" }, add_opts))
-			Insp(vim.tbl_extend("keep", { type = "autosnippets" }, add_opts))
-			ls.add_snippets(
-				ft,
-				snippets,
-				vim.tbl_extend("keep", { type = "snippets" }, add_opts)
-			)
-			ls.add_snippets(
-				ft,
-				autosnippets,
-				vim.tbl_extend("keep", { type = "autosnippets" }, add_opts)
-			)
+			-- instantly load snippets if the ft is already loaded...
+			load_snippet_files(ft, paths, collection_paths, add_opts)
 			-- clear from load_paths to prevent duplicat loads.
 			load_paths[ft] = nil
 		end
