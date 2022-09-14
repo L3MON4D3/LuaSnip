a290afdd82a4136e6ab918efaf7141f3f9ef8bf4
Thore Strassburg <thore@weilbier.net>
2022-09-14 08:35:40 +0200

    feat: add new built-in condition for line ending

--- lua/luasnip/extras/expand_conditions.lua
+++ lua/luasnip/extras/expand_conditions.lua
@@ -5,4 +5,9 @@ function M.line_begin(line_to_cursor, matched_trigger)
 	return line_to_cursor:sub(1, -(#matched_trigger + 1)):match("^%s*$")
 end
 
+function M.line_end(line_to_cursor)
+	local line = vim.api.nvim_get_current_line()
+	return #line_to_cursor == #line
+end
+
 return M
