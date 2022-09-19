4e1337443f1a04a1edc88f9b9b755ec4e8e8c228
atticus-sullivan <mail-login+gitlab@protonmail.com>
2022-09-19 21:55:54 +0200

    fiction example for invalidation function

--- lua/luasnip/extras/expand_conditions.lua
+++ lua/luasnip/extras/expand_conditions.lua
@@ -24,28 +24,42 @@ local memoization_mt = {
 -- low level factory
 -- invalidate(table) -> bool: decides if the memoization should be invalidated,
 -- can store state in table
--- TODO provide invalidate defaults (buffer, cursor, changes, none)
 function M.memoization_factory(func, invalidate)
 	-- always invalidare by default
 	invalidate = invalidate or function() return true end
 	return setmetatable({func=func, invalidate=invalidate}, memoization_mt)
 end
 
--------------
--- PRESETS --
--------------
+-------------------
+-- INVAL PRESETS --
+-------------------
+-- TODO provide invalidate defaults (buffer, cursor, changes, none)
+M.inval = {}
+-- just a small example to illustrate the usage (will most probably have to be
+-- changed)
+function M.inval.line(tab, _, _, _, line)
+	return tab.line ~= line
+end
+
+------------------
+-- COND PRESETS --
+------------------
 local function line_begin(line_to_cursor, matched_trigger)
 	-- +1 because `string.sub("abcd", 1, -2)` -> abc
 	return line_to_cursor:sub(1, -(#matched_trigger + 1)):match("^%s*$")
 end
-function M.line_begin() return M.memoization_factory(line_begin, nil) -- TODO invalidation function
+function M.line_begin()
+	-- TODO invalidation function
+	return M.memoization_factory(line_begin, nil)
 end
 
 local function line_end(line_to_cursor)
 	local line = vim.api.nvim_get_current_line()
 	return #line_to_cursor == #line
 end
-function M.line_end() return M.memoization_factory(line_end, nil) -- TODO invalidation function
+function M.line_end()
+	-- TODO invalidation function
+	return M.memoization_factory(line_end, nil)
 end
 
 return M
