bd60b3ffe41a719a1026539c84e37418477de719
Uy Ha <hchanuy@gmail.com>
2022-12-01 20:47:01 +0100

    feat(extras): insert a repeat node based on the position of the previously occured node instead of a copy of it
    don't have implementation rely on tests/handle tests specially.
    
    adapt existing tests.
    
    Provide objects on which `get_jump_index` can be called, as opposed to
    flat numbers.

--- tests/unit/fmt_spec.lua
+++ tests/unit/fmt_spec.lua
@@ -1,20 +1,46 @@
 local helpers = require("test.functional.helpers")(after_each)
 
-local works = function(msg, fmt, args, expected, opts)
-	it(msg, function()
+---@param params { msg: string, fmt: string, args: string, expected: string, opts: string, prolouge: string}
+local works = function(params)
+	it(params.msg, function()
 		-- normally `exec_lua` accepts a table which is passed to the code as `...`, but it
 		-- fails if number- and string-keys are mixed ({1,2} works fine, {1, b=2} fails).
 		-- So we limit ourselves to just passing strings, which are then turned into tables
 		-- while load()ing the function.
 		local result = helpers.exec_lua(
 			string.format(
-				'return require("luasnip.extras.fmt").interpolate("%s", %s, %s)',
-				fmt,
-				args,
-				opts
+				[[
+						local args = %s
+						local mock_args = {}
+
+						local fake_metatable = {
+							__tostring = function(self)
+								return self.value
+							end,
+							get_jump_index = function(self) return nil end
+						}
+						fake_metatable.__index = fake_metatable
+
+						local fake_node = function(value)
+							return setmetatable({value = value}, fake_metatable)
+						end
+						for key, arg in pairs(args) do
+							mock_args[key] = fake_node(arg)
+						end
+						local result = require("luasnip.extras.fmt").interpolate("%s", mock_args, %s)
+
+						local str_result = ""
+						for _, value in pairs(result) do
+							str_result = str_result .. tostring(value)
+						end
+						return str_result
+						]],
+				params.args,
+				params.fmt,
+				params.opts
 			)
 		)
-		assert.are.same(expected, table.concat(result))
+		assert.are.same(params.expected, result)
 	end)
 end
 
@@ -39,118 +65,145 @@ describe("fmt.interpolate", function()
 	-- set in makefile.
 	helpers.exec("set rtp+=" .. os.getenv("LUASNIP_SOURCE"))
 
-	works("expands with no numbers", "a{}b{}c{}d", "{ 4, 5, 6 }", "a4b5c6d")
+	works({
+		msg = "expands with no numbers",
+		fmt = "a{}b{}c{}d",
+		args = "{ 4, 5, 6 }",
+		expected = "a4b5c6d",
+	})
 
-	works(
-		"expands with explicit numbers",
-		"a{2}b{1}c{3}d",
-		"{ 4, 5, 6 }",
-		"a5b4c6d"
-	)
+	works({
+		msg = "expands with explicit numbers",
+		fmt = "a{2}b{1}c{3}d",
+		args = "{ 4, 5, 6 }",
+		expected = "a5b4c6d",
+	})
 
-	works(
-		"expands with mixed numbering",
-		"a{}b{3}c{}d{2}e",
-		"{ 1, 2, 3, 4 }",
-		"a1b3c4d2e"
-	)
+	works({
+		msg = "expands with mixed numbering",
+		fmt = "a{}b{3}c{}d{2}e",
+		args = "{ 1, 2, 3, 4 }",
+		expected = "a1b3c4d2e",
+	})
 
-	works(
-		"expands named placeholders",
-		"a{A}b{B}c{C}d",
-		"{ A = 1, B = 2, C = 3 }",
-		"a1b2c3d"
-	)
+	works({
+		msg = "expands named placeholders",
+		fmt = "a{A}b{B}c{C}d",
+		args = "{ A = 1, B = 2, C = 3 }",
+		expected = "a1b2c3d",
+	})
 
-	works(
-		"expands all mixed",
-		"a {A} b {} c {3} d {} e {B} f {A} g {2} h",
-		"{ 1, 2, 3, 4, A = 10, B = 20 }",
-		"a 10 b 1 c 3 d 4 e 20 f 10 g 2 h"
-	)
+	works({
+		msg = "expands all mixed",
+		fmt = "a {A} b {} c {3} d {} e {B} f {A} g {2} h",
+		args = "{ 1, 2, 3, 4, A = 10, B = 20 }",
+		expected = "a 10 b 1 c 3 d 4 e 20 f 10 g 2 h",
+	})
 
-	works(
-		"current index changed by numbered nodes",
-		"{} {} {1} {} {}",
-		"{ 1, 2, 3 }",
-		"1 2 1 2 3"
-	)
+	works({
+		msg = "current index changed by numbered nodes",
+		fmt = "{} {} {1} {} {}",
+		args = "{ 1, 2, 3 }",
+		expected = "1 2 1 2 3",
+	})
 
-	works("excludes trailing text", "{}abcd{}", "{ 1, 2 }", "1abcd2")
+	works({
+		msg = "excludes trailing text",
+		fmt = "{}abcd{}",
+		args = "{ 1, 2 }",
+		expected = "1abcd2",
+	})
 
-	works(
-		"escapes empty double-braces",
-		"a{{}}b{}c{{}}d{}e",
-		"{ 2, 4 }",
-		"a{}b2c{}d4e"
-	)
+	works({
+		msg = "escapes empty double-braces",
+		fmt = "a{{}}b{}c{{}}d{}e",
+		args = "{ 2, 4 }",
+		expected = "a{}b2c{}d4e",
+	})
 
-	works("escapes non-empty double-braces", "a{{d}}b{}c", "{ 2 }", "a{d}b2c")
+	works({
+		msg = "escapes non-empty double-braces",
+		fmt = "a{{d}}b{}c",
+		args = "{ 2 }",
+		expected = "a{d}b2c",
+	})
 
-	works(
-		"do not trim placeholders with whitespace",
-		"a{ something}b{}c",
-		'{ 2, [" something"] = 1 }',
-		"a1b2c"
-	)
+	works({
+		msg = "do not trim placeholders with whitespace",
+		fmt = "a{ something}b{}c",
+		args = '{ 2, [" something"] = 1 }',
+		expected = "a1b2c",
+	})
 
-	works(
-		"replaces nested escaped braces",
-		"a{{{{}}}}b{}c{{ {{ }}}}d",
-		"{ 2 }",
-		"a{{}}b2c{ { }}d"
-	)
+	works({
+		msg = "replaces nested escaped braces",
+		fmt = "a{{{{}}}}b{}c{{ {{ }}}}d",
+		args = "{ 2 }",
+		expected = "a{{}}b2c{ { }}d",
+	})
 
-	works("replaces umatched escaped braces", "a{{{{b{}c", "{ 2 }", "a{{b2c")
+	works({
+		msg = "replaces umatched escaped braces",
+		fmt = "a{{{{b{}c",
+		args = "{ 2 }",
+		expected = "a{{b2c",
+	})
 
-	works(
-		"replaces in braces inside escaped braces",
-		"a{{{}}}b{{ {}}}c{{{} }}d{{ {} }}e",
-		"{ 1, 2, 3, 4 }",
-		"a{1}b{ 2}c{3 }d{ 4 }e"
-	)
+	works({
+		msg = "replaces in braces inside escaped braces",
+		fmt = "a{{{}}}b{{ {}}}c{{{} }}d{{ {} }}e",
+		args = "{ 1, 2, 3, 4 }",
+		expected = "a{1}b{ 2}c{3 }d{ 4 }e",
+	})
+
+	works({
+		msg = "repeats node with default options",
+		fmt = "{a}{a}",
+		args = "{a = 1}",
+		expected = "11",
+	})
 
 	fails("fails for unbalanced braces", "a{b", {})
 
 	fails("fails for nested braces", "a{ { } }b", {})
 
-	works(
-		"can use different delimiters",
-		"foo() { return <>; };",
-		"{ 10 }",
-		"foo() { return 10; };",
-		'{ delimiters = "<>" }'
-	)
+	works({
+		msg = "can use different delimiters",
+		fmt = "foo() { return <>; };",
+		args = "{ 10 }",
+		expected = "foo() { return 10; };",
+		opts = '{ delimiters = "<>" }',
+	})
 
 	local delimiters = { "()", "[]", "<>", "%$", "#@", "?!" }
 	for _, delims in ipairs(delimiters) do
 		local left, right = delims:sub(1, 1), delims:sub(2, 2)
 		describe("can use custom delimiters", function()
-			works(
-				delims,
-				string.format("{ return %s%s; };", left, right),
-				"{ 10 }",
-				"{ return 10; };",
-				string.format('{ delimiters = "%s" }', delims)
-			)
+			works({
+				msg = delims,
+				fmt = string.format("{ return %s%s; };", left, right),
+				args = "{ 10 }",
+				expected = "{ return 10; };",
+				opts = string.format('{ delimiters = "%s" }', delims),
+			})
 		end)
 	end
 
-	works(
-		"can escape custom delimiters",
-		"foo((x)) { return x + (); };",
-		"{ 10 }",
-		"foo(x) { return x + 10; };",
-		'{ delimiters = "()" }'
-	)
+	works({
+		msg = "can escape custom delimiters",
+		fmt = "foo((x)) { return x + (); };",
+		args = "{ 10 }",
+		expected = "foo(x) { return x + 10; };",
+		opts = '{ delimiters = "()" }',
+	})
 
-	works(
-		"can use named placeholders with custom delimiters",
-		"foo(x) { return x + [y]; };",
-		"{ y = 10 }",
-		"foo(x) { return x + 10; };",
-		'{ delimiters = "[]" }'
-	)
+	works({
+		msg = "can use named placeholders with custom delimiters",
+		fmt = "foo(x) { return x + [y]; };",
+		args = "{ y = 10 }",
+		expected = "foo(x) { return x + 10; };",
+		opts = '{ delimiters = "[]" }',
+	})
 
 	fails("dissallows unused list args", "a {} b {} c", "{ 1, 2, 3 }")
 
@@ -160,11 +213,11 @@ describe("fmt.interpolate", function()
 		"{ 1, A = 10, B = 20, C = 30 }"
 	)
 
-	works(
-		"allows unused with strict=false",
-		"a {A} b {B} c {} d",
-		"{ 1, 2, A = 10, B = 20, C = 30 }",
-		"a 10 b 20 c 1 d",
-		"{ strict = false }"
-	)
+	works({
+		msg = "allows unused with strict=false",
+		fmt = "a {A} b {B} c {} d",
+		args = "{ 1, 2, A = 10, B = 20, C = 30 }",
+		expected = "a 10 b 20 c 1 d",
+		opts = "{ strict = false }",
+	})
 end)
