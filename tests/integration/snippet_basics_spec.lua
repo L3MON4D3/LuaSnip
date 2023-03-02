L3MON4D3/LuaSnip

commit 9c659c3239b0ea83e904cc66746a0ebf6c2873c5

refactor: use autocmd lua apis.

lua/luasnip/config.lua
@@ -50,8 +50,8 @@ local defaults = {
 	history = false,
 	update_events = "InsertLeave",
 	-- see :h User, event should never be triggered(except if it is `doautocmd`'d)
-	region_check_events = "User None",
-	delete_check_events = "User None",
+	region_check_events = nil,
+	delete_check_events = nil,
 	store_selection_keys = nil, -- Supossed to be the same as the expand shortcut
 	ext_opts = {
 		[types.textNode] = {
@@ -215,29 +215,49 @@ c = {
 	end,
 
 	_setup = function()
-		vim.cmd(
-			string.format(
-				[[
-        augroup luasnip
-            au!
-            autocmd %s * lua require("luasnip").unlink_current_if_deleted()
-            autocmd %s * lua require("luasnip").active_update_dependents()
-            autocmd %s * lua require("luasnip").exit_out_of_region(require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()])
-			"Remove buffers' nodes on deletion+wipeout.
-			autocmd BufDelete,BufWipeout * lua current_nodes = require("luasnip").session.current_nodes if current_nodes then current_nodes[tonumber(vim.fn.expand("<abuf>"))] = nil end
-		]]
-					.. (session.config.enable_autosnippets and [[
-			autocmd InsertCharPre * lua Luasnip_just_inserted = true
-			autocmd TextChangedI,TextChangedP * lua if Luasnip_just_inserted then require("luasnip").expand_auto() Luasnip_just_inserted=nil end
-		]] or "")
-					.. [[
-		augroup END
-		]],
+		local augroup = vim.api.nvim_create_augroup("luasnip", {})
+		local function ls_autocmd(event, callback)
+			vim.api.nvim_create_autocmd(event, {
+				callback = callback,
+				group = augroup,
+			})
+		end
+		if session.config.delete_check_events ~= nil then
+			ls_autocmd(
 				session.config.delete_check_events,
-				session.config.update_events,
-				session.config.region_check_events
+				require("luasnip").unlink_current_if_deleted
 			)
+		end
+		ls_autocmd(
+			session.config.update_events,
+			require("luasnip").active_update_dependents
 		)
+		if session.config.region_check_events ~= nil then
+			ls_autocmd(session.config.region_check_events, function()
+				require("luasnip").exit_out_of_region(
+					require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+				)
+			end)
+		end
+		-- Remove buffers' nodes on deletion+wipeout.
+		ls_autocmd({ "BufDelete", "BufWipeout" }, function()
+			local current_nodes = require("luasnip").session.current_nodes
+			if current_nodes then
+				current_nodes[tonumber(vim.fn.expand("<abuf>"))] = nil
+			end
+		end)
+		if session.config.enable_autosnippets then
+			ls_autocmd("InsertCharPre", function()
+				Luasnip_just_inserted = true
+			end)
+			ls_autocmd({ "TextChangedI", "TextChangedP" }, function()
+				if Luasnip_just_inserted then
+					require("luasnip").expand_auto()
+					Luasnip_just_inserted = nil
+				end
+			end)
+		end
+
 		if session.config.store_selection_keys then
 			vim.cmd(
 				string.format(

tests/integration/snippet_basics_spec.lua
@@ -558,4 +558,68 @@ describe("snippets_basic", function()
 			)
 		)
 	end)
+
+	it("autocommands are triggered by events", function()
+		assert.is_true(exec_lua([[
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LuasnipInsertNodeEnter",
+				callback = function()
+					inode_did_enter = true
+				end
+			})
+			ls.snip_expand(
+				s("trig", {
+					t"text", i(1), t"text again", i(2), t"and again"
+				}) )
+			return inode_did_enter
+		]]))
+	end)
+
+	it("{region,delete}_check_events works correctly", function()
+		exec_lua([[
+			ls.setup({
+				history = true,
+				region_check_events = {"CursorHold", "InsertLeave"},
+				delete_check_events = "TextChanged,InsertEnter",
+			})
+
+			ls.snip_expand(s("a", {
+				t"sometext", i(1, "someinsertnode")
+			}))
+		]])
+screen:expect{grid=[[
+  sometext^s{3:omeinsertnode}                            |
+  {0:~                                                 }|
+  {2:-- SELECT --}                                      |
+]]}
+		-- leave snippet-area, and trigger insertLeave.
+		feed("<Esc>o<Esc>")
+screen:expect{grid=[[
+  sometextsomeinsertnode                            |
+  ^                                                  |
+                                                    |
+]]}
+		-- make sure we're in the last tabstop (ie. region_check_events did its
+		-- job).
+		exec_lua("ls.jump(1)")
+screen:expect{grid=[[
+  sometextsomeinsertnode                            |
+  ^                                                  |
+                                                    |
+]]}
+		-- not really necessary, but feels safer this way.
+		exec_lua("ls.jump(-1)")
+screen:expect{grid=[[
+  sometext^s{3:omeinsertnode}                            |
+                                                    |
+  {2:-- SELECT --}                                      |
+]]}
+
+		-- delete snippet text
+		feed("<Esc>dd")
+		-- make sure the snippet is no longer active.
+		assert.is_true(exec_lua([[
+			return ls.session.current_nodes[vim.api.nvim_get_current_buf()] == nil
+		]]))
+	end)
 end)

lua/luasnip/init.lua
@@ -616,7 +616,10 @@ end
 
 local function cleanup()
 	-- Use this to reload luasnip
-	vim.cmd([[doautocmd <nomodeline> User LuasnipCleanup]])
+	vim.api.nvim_exec_autocmds(
+		"User",
+		{ pattern = "LuasnipCleanup", modeline = false }
+	)
 	-- clear all snippets.
 	snippet_collection.clear_snippets()
 	loader.cleanup()
@@ -634,7 +637,10 @@ local function refresh_notify(ft)
 		end
 	else
 		session.latest_load_ft = ft
-		vim.cmd([[doautocmd <nomodeline> User LuasnipSnippetsAdded]])
+		vim.api.nvim_exec_autocmds(
+			"User",
+			{ pattern = "LuasnipSnippetsAdded", modeline = false }
+		)
 	end
 end
 

lua/luasnip/loaders/init.lua
@@ -104,11 +104,12 @@ function M.load_lazy_loaded(fts)
 	end
 end
 
-vim.cmd([[
-		augroup luasnip_watch_reload
-		autocmd BufWritePost * lua require("luasnip.loaders").reload_file(vim.fn.expand("<afile>"))
-		augroup END
-	]])
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = vim.api.nvim_create_augroup("luasnip_watch_reload", {}),
+	callback = function()
+		require("luasnip.loaders").reload_file(vim.fn.expand("<afile>"))
+	end,
+})
 function M.reload_file(filename)
 	filename = Path.normalize(filename)
 	if not filename then

lua/luasnip/nodes/node.lua
@@ -250,9 +250,12 @@ function Node:event(event)
 	end
 
 	session.event_node = self
-	vim.cmd(
-		"doautocmd <nomodeline> User Luasnip"
-			.. events.to_string(self.type, event)
+	vim.api.nvim_exec_autocmds(
+		"User",
+		{
+			pattern = "Luasnip" .. events.to_string(self.type, event),
+			modeline = false,
+		}
 	)
 end
 

lua/luasnip/nodes/snippet.lua
@@ -1042,9 +1042,12 @@ function Snippet:event(event, event_args)
 
 	session.event_node = self
 	session.event_args = event_args
-	vim.cmd(
-		"doautocmd <nomodeline> User Luasnip"
-			.. events.to_string(self.type, event)
+	vim.api.nvim_exec_autocmds(
+		"User",
+		{
+			pattern = "Luasnip" .. events.to_string(self.type, event),
+			modeline = false,
+		}
 	)
 
 	return cb_res

commit 5a48c7e22f2c56aa7d8412e1b327d054c917286d

fix: accept ","-delimited events in config (close #801).
broken by 9c659c3, only the first event in the ,-delimited list is
accepted, not causing a test-failure.

lua/luasnip/config.lua
@@ -216,8 +216,15 @@ c = {
 
 	_setup = function()
 		local augroup = vim.api.nvim_create_augroup("luasnip", {})
-		local function ls_autocmd(event, callback)
-			vim.api.nvim_create_autocmd(event, {
+
+		-- events: string[], or string. if string[], each string is one
+		-- event-name, if string, either one event-name, or multiple delimited by `,`.
+		local function ls_autocmd(events, callback)
+			if type(events) == "string" then
+				-- split on ',' for backwards compatibility.
+				events = vim.split(events, ",")
+			end
+			vim.api.nvim_create_autocmd(events, {
 				callback = callback,
 				group = augroup,
 			})

tests/integration/snippet_basics_spec.lua
@@ -631,6 +631,43 @@ describe("snippets_basic", function()
 		]]))
 	end)
 
+	it("autocommands are registered in different formats", function()
+		exec_lua([[
+			a_set = false
+			b_set = false
+			c_set = false
+
+			local function set_a() a_set = true end
+			local function set_b() b_set = true end
+			local function set_c() c_set = true end
+
+			ls.exit_out_of_region = set_a
+			ls.unlink_current_if_deleted = set_b
+			ls.active_update_dependents = set_c
+
+			ls.setup({
+				region_check_events = {"InsertLeave","CursorHold"},
+				delete_check_events = "TextChanged,InsertEnter",
+				update_events = {"TextChanged"},
+			})
+
+			vim.api.nvim_exec_autocmds("InsertLeave", {})
+			assert(a_set)
+			a_set = false
+			vim.api.nvim_exec_autocmds("CursorHold", {})
+			assert(a_set)
+
+			vim.api.nvim_exec_autocmds("TextChanged", {})
+			assert(b_set)
+			b_set = false
+			vim.api.nvim_exec_autocmds("InsertEnter", {})
+			assert(b_set)
+
+			vim.api.nvim_exec_autocmds("TextChanged", {})
+			assert(c_set)
+		]])
+	end)
+
 	it(
 		"jump_destination works for snippets where `node.active` is important",
 		function()