7cbbe4a8319cf6e35ecfa13e29dc9fec263469f8
Serdar Sayın <25414028+ssayin@users.noreply.github.com>
2023-05-05 16:17:35 +0300

    fix: vim.pretty_print is deprecated (^neovim-0.9.0)
    see:
    https://github.com/neovim/neovim/blob/040f1459849ab05b04f6bb1e77b3def16b4c2f2b/runtime/doc/news.txt#LL321C3-L321C3
    
    vim.pretty_print is removed in development (prerelease) build:
    https://github.com/neovim/neovim/releases/tag/nightly

--- tests/integration/snippet_basics_spec.lua
+++ tests/integration/snippet_basics_spec.lua
@@ -768,4 +768,71 @@ describe("snippets_basic", function()
 			end
 		end
 	)
+
+	it("LuaSnipListAvailable works", function()
+		helpers.clear()
+		ls_helpers.session_setup_luasnip()
+
+		screen = Screen.new(50, 40)
+		screen:attach()
+		screen:set_default_attr_ids({
+			[0] = {bold = true, foreground = Screen.colors.Blue1};
+			[1] = {bold = true, foreground = Screen.colors.Brown};
+			[2] = {bold = true};
+			[3] = {background = Screen.colors.LightGrey};
+			[4] = {bold = true, reverse = true};
+			[5] = {bold = true, foreground = Screen.colors.SeaGreen4};
+			[6] = {foreground = Screen.colors.Red1};
+		})
+		exec_lua([[
+			ls.add_snippets("all", {
+				s({trig="a"}, { t"justsometexta" }),
+				s({trig="b"}, { t"justsometextb" }),
+				s({trig="c"}, { t"justsometextc" }),
+			})
+		]])
+		feed(":LuaSnipListAvailable<Cr>")
+		screen:expect{grid=[[
+			                                                  |
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{0:~                                                 }|
+			{4:                                                  }|
+			{                                                 |
+			  [""] = {},                                      |
+			  all = { {                                       |
+			      description = { "a" },                      |
+			      name = "a",                                 |
+			      regTrig = false,                            |
+			      trigger = "a",                              |
+			      wordTrig = true                             |
+			    }, {                                          |
+			      description = { "b" },                      |
+			      name = "b",                                 |
+			      regTrig = false,                            |
+			      trigger = "b",                              |
+			      wordTrig = true                             |
+			    }, {                                          |
+			      description = { "c" },                      |
+			      name = "c",                                 |
+			      regTrig = false,                            |
+			      trigger = "c",                              |
+			      wordTrig = true                             |
+			    } }                                           |
+			}                                                 |
+			{5:Press ENTER or type command to continue}^           |]]}
+		feed("<Cr>")
+	end)
 end)
