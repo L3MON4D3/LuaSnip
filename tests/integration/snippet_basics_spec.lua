2c3a3a3e4fb503bf39efb61290ecfa8aae95f5eb
L3MON4D3 <L3MON4D3@users.noreply.github.com>
2023-10-04 17:12:20 +0000

    Format with stylua

--- tests/integration/snippet_basics_spec.lua
+++ tests/integration/snippet_basics_spec.lua
@@ -1343,17 +1343,21 @@ describe("snippets_basic", function()
 	it("unlink_current works.", function()
 		exec_lua([[ls.lsp_expand("$1 adsf $2")]])
 		exec_lua([[ls.jump( 1)]])
-		screen:expect{grid=[[
+		screen:expect({
+			grid = [[
 			 adsf ^                                            |
 			{0:~                                                 }|
-			{2:-- INSERT --}                                      |]]}
+			{2:-- INSERT --}                                      |]],
+		})
 		exec_lua([[ls.jump(-1)]])
-		screen:expect{grid=[[
+		screen:expect({
+			grid = [[
 			^ adsf                                             |
 			{0:~                                                 }|
-			{2:-- INSERT --}                                      |]]}
+			{2:-- INSERT --}                                      |]],
+		})
 		exec_lua([[ls.unlink_current()]])
 		exec_lua([[ls.jump( 1)]])
-		screen:expect{unchanged=true}
+		screen:expect({ unchanged = true })
 	end)
 end)
