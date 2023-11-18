52918849e2f2ba0f2c3329598d401ad13d6167ea
L3MON4D3 <Simljk@outlook.de>
2023-11-18 21:28:05 +0100

    rename digraph-operations.
    calling add_edge twice with the same vertices will not connect the
    vertices twice.

--- lua/luasnip/util/directed_graph.lua
+++ lua/luasnip/util/directed_graph.lua
@@ -1,3 +1,6 @@
+local tbl_util = require("luasnip.util.table")
+local autotable = require("luasnip.util.auto_table").autotable
+
 local DirectedGraph = {}
 
 -- set __index directly in DirectedGraph, otherwise each DirectedGraph-object would have its'
@@ -24,7 +27,7 @@ local function new_vertex()
 end
 
 ---Add new vertex to the DirectedGraph
----@return table: the generated vertex, to be used in `add_edge`, for example.
+---@return table: the generated vertex, to be used in `set_edge`, for example.
 function DirectedGraph:add_vertex()
 	local vert = new_vertex()
 	table.insert(self.vertices, vert)
@@ -33,7 +36,7 @@ end
 
 ---Remove vertex and its edges from DirectedGraph.
 ---@param v table: the vertex.
-function DirectedGraph:remove_vertex(v)
+function DirectedGraph:clear_vertex(v)
 	if not vim.tbl_contains(self.vertices, v) then
 		-- vertex does not belong to this graph. Maybe throw error/make
 		-- condition known?
@@ -41,18 +44,18 @@ function DirectedGraph:remove_vertex(v)
 	end
 	-- remove outgoing..
 	for outgoing_edge_vert, _ in pairs(v.outgoing_edge_verts) do
-		self:remove_edge(v, outgoing_edge_vert)
+		self:clear_edge(v, outgoing_edge_vert)
 	end
 	-- ..and incoming edges with v from the graph.
 	for incoming_edge_vert, _ in pairs(v.incoming_edge_verts) do
-		self:remove_edge(incoming_edge_vert, v)
+		self:clear_edge(incoming_edge_vert, v)
 	end
 end
 
 ---Add edge from v1 to v2
 ---@param v1 table: vertex in the graph.
 ---@param v2 table: vertex in the graph.
-function DirectedGraph:add_edge(v1, v2)
+function DirectedGraph:set_edge(v1, v2)
 	if v1.outgoing_edge_verts[v2] then
 		-- the edge already exists. Don't return an error, for now.
 		return
@@ -65,7 +68,7 @@ end
 ---Remove edge from v1 to v2
 ---@param v1 table: vertex in the graph.
 ---@param v2 table: vertex in the graph.
-function DirectedGraph:remove_edge(v1, v2)
+function DirectedGraph:clear_edge(v1, v2)
 	assert(v1.outgoing_edge_verts[v2], "nonexistent edge cannot be removed.")
 	-- unlink vertices.
 	v1.outgoing_edge_verts[v2] = nil
@@ -135,7 +138,7 @@ function DirectedGraph:topological_sort()
 		end
 
 		-- finally: remove v from graph and sources.
-		graph:remove_vertex(v)
+		graph:clear_vertex(v)
 	end
 
 	if #sorting ~= #self.vertices then
@@ -145,4 +148,117 @@ function DirectedGraph:topological_sort()
 	return sorting
 end
 
-return { new = new_graph }
+-- return all vertices reachable from this one.
+function DirectedGraph:connected_component(vert, edge_direction)
+	local outgoing_vertices_field =
+		edge_direction == "Backward"
+			and "incoming_edge_verts"
+			 or "outgoing_edge_verts"
+
+	local visited = {}
+	local to_visit = {[vert] = true}
+
+	-- get any value in table.
+	local next_vert, _ = next(to_visit, nil)
+	while next_vert do
+		to_visit[next_vert] = nil
+		visited[next_vert] = true
+
+		for neighbor, _ in pairs(next_vert[outgoing_vertices_field]) do
+			if not visited[neighbor] then
+				to_visit[neighbor] = true
+			end
+		end
+
+		next_vert, _ = next(to_visit, nil)
+	end
+
+	return tbl_util.set_to_list(visited)
+end
+
+-- Very useful to have a graph where vertices are associated with some label.
+-- This just proxies DirectedGraph and swaps labels and vertices in
+-- parameters/return-values.
+local LabeledDigraph = {}
+LabeledDigraph.__index = LabeledDigraph
+
+local function new_labeled_graph()
+	return setmetatable({
+		graph = new_graph(),
+		label_to_vert = {},
+		vert_to_label = {},
+		-- map label -> origin-vert -> dest-vert
+		label_to_verts = autotable(3, {warn = false}),
+		-- map edge (origin,dest) to set of labels.
+		verts_to_label = autotable(3, {warn = false})
+	}, LabeledDigraph)
+end
+
+function LabeledDigraph:set_vertex(label)
+	if self.label_to_vert[label] then
+		-- don't add same label again.
+		return
+	end
+
+	local vert = self.graph:add_vertex()
+	self.label_to_vert[label] = vert
+	self.vert_to_label[vert] = label
+end
+
+function LabeledDigraph:set_edge(lv1, lv2, edge_label)
+	-- ensure vertices exist.
+	self:set_vertex(lv1)
+	self:set_vertex(lv2)
+
+	if self.verts_to_label[lv1][lv2][edge_label] then
+		-- edge exists, do nothing.
+		return
+	end
+
+	-- determine before setting the lv1-lv2-edge.
+	local other_edge_exists = next(self.verts_to_label[lv1][lv2], nil) ~= nil
+
+	-- store both associations; 
+	self.verts_to_label[lv1][lv2][edge_label] = true
+	self.label_to_verts[edge_label][lv1][lv2] = true
+
+	if other_edge_exists then
+		-- there already exists an entry for this edge, no need to add it to
+		-- the graph.
+		return
+	end
+	self.graph:set_edge(self.label_to_vert[lv1], self.label_to_vert[lv2])
+end
+
+function LabeledDigraph:clear_edge(lv1, lv2, ledge)
+	if not self.verts_to_label[lv1][lv2][ledge] then
+		-- edge does not exist, do nothing.
+		return
+	end
+
+	self.verts_to_label[lv1][lv2][ledge] = nil
+	if next(self.verts_to_label[lv1][lv2]) == nil then
+		-- removed last edge between v1, v2 -> remove edge in graph.
+		self.graph:clear_edge(self.label_to_vert[lv1], self.label_to_vert[lv2])
+	end
+end
+function LabeledDigraph:clear_edges(label)
+	for lv1, lv2 in pairs(self.label_to_verts[label]) do
+		self:clear_edge(lv1, lv2, label)
+	end
+	-- set to nil, not {}, so autotable can work its magic.
+	self.label_to_verts[label] = nil
+end
+
+function LabeledDigraph:connected_component(lv, edge_direction)
+	self:set_vertex(lv)
+
+	return vim.tbl_map(function(v)
+		return self.vert_to_label[v]
+	end, self.graph:connected_component(self.label_to_vert[lv], edge_direction))
+end
+
+return {
+	new = new_graph,
+	new_labeled = new_labeled_graph
+}
