39c0b5b6b3a3afe65052ecfde12326fd13ad25d2
L3MON4D3 <Simljk@outlook.de>
2023-11-18 21:22:16 +0100

    extract autotables into their own module.

--- lua/luasnip/session/snippet_collection/init.lua
+++ lua/luasnip/session/snippet_collection/init.lua
@@ -1,4 +1,5 @@
 local source = require("luasnip.session.snippet_collection.source")
+local auto_creating_tables = require("luasnip.util.auto_table").warn_depth_autotable
 
 -- store snippets by some key.
 -- also ordered by filetype, eg.
@@ -11,28 +12,6 @@ local source = require("luasnip.session.snippet_collection.source")
 local M = {
 	invalidated_count = 0,
 }
-
--- depth specifies how many levels under this table should be allowed to index
--- throug this metamethod
--- set depth to 0 to disable checking
--- Acknowledgment: This is (maybe more than) inspired by
--- https://lua-users.org/wiki/AutomagicTables so special thanks to
--- Thomas Wrensch and Rici Lake for sharing their ideas on this topic.
-local function auto_creating_tables(self, key, depth)
-	local t = {}
-	assert(depth ~= 1, "don't index at that level")
-	setmetatable(t, {
-		-- creating a new function on each time (could be shared) isn't that
-		-- nice. Nonetheless this shouldn't be too bad, as these are only
-		-- created twice (auto+snippet) per ft and twice for each prio,ft
-		-- combination
-		__index = function(s, k)
-			return auto_creating_tables(s, k, depth - 1)
-		end,
-	})
-	self[key] = t
-	return t
-end
 
 local by_key = {}
 
