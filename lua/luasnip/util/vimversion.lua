1ab7c7f4301ba2280233ecd9b765d193b799773b
L3MON4D3 <L3MON4D3@users.noreply.github.com>
2024-10-23 13:23:36 +0000

    Format with stylua

--- lua/luasnip/util/vimversion.lua
+++ lua/luasnip/util/vimversion.lua
@@ -3,9 +3,13 @@ local M = {}
 function M.ge(maj, min, patch)
 	local version = vim.version()
 	return
-		(version.major > maj) or
-		(version.major == maj and version.minor > min) or
-		(version.major == maj and version.minor == min and version.patch >= patch)
+		(version.major > maj)
+			or (version.major == maj and version.minor > min)
+			or (
+				version.major == maj
+				and version.minor == min
+				and version.patch >= patch
+			)
 end
 
 return M
