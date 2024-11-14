29c1ee24e74c77f4cffe18add7155c3c6344af62
L3MON4D3 <Simljk@outlook.de>
2024-11-14 12:20:02 +0100

    add a few tests for previous changes.

--- tests/integration/restore_spec.lua
+++ tests/integration/restore_spec.lua
@@ -461,7 +461,7 @@ screen:expect({
 	end)
 
 	-- make sure store and update_restore propagate.
-	it("correctly restores snippets (3).", function()
+	it("correctly restores snippets (4).", function()
 
 		exec_lua([[
 			ls.setup({link_children = true})
