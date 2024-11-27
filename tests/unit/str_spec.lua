71665e7d9bfb67dffd84090b2fca6e47cf49b100
xudyang1 <61672396+xudyang1@users.noreply.github.com>
2024-11-27 16:43:53 -0500

    feat(fmt): enable indent string conversion

--- tests/unit/str_spec.lua
+++ tests/unit/str_spec.lua
@@ -60,3 +60,138 @@ describe("str.unescaped_pairs", function()
 		{ { 1, 3 }, { 5, 8 }, { 9, 11 } }
 	)
 end)
+
+describe("str.dedent", function()
+	-- apparently clear() needs to run before anything else...
+	ls_helpers.clear()
+	exec("set rtp+=" .. os.getenv("LUASNIP_SOURCE"))
+	local function get_dedent_result(input_string)
+		local result = exec_lua(
+			string.format(
+				[[return require("luasnip.util.str").dedent("%s")]],
+				input_string
+			)
+		)
+		return result
+	end
+
+	it("spaces at beginnig", function()
+		local input_table = {
+			"  line1",
+			"  ",
+			"    line3",
+			"      line4",
+		}
+		local input_string = table.concat(input_table, [[\n]])
+		local expect_table = {
+			"line1",
+			"",
+			"  line3",
+			"    line4",
+		}
+		local expected = table.concat(expect_table, "\n")
+
+		local result = get_dedent_result(input_string)
+
+		assert.are.same(expected, result)
+	end)
+	it("tabs at beginnig", function()
+		local input_table = {
+			[[\t\tline1]],
+			[[\t\t]],
+			[[\t\t\tline3]],
+			[[\t\t\t\tline4]],
+		}
+		local input_string = table.concat(input_table, [[\n]])
+		local expect_table = {
+			"line1",
+			"",
+			"\tline3",
+			"\t\tline4",
+		}
+		local expected = table.concat(expect_table, "\n")
+
+		local result = get_dedent_result(input_string)
+
+		assert.are.same(expected, result)
+	end)
+	it("tabs & spaces at beginnig", function()
+		local input_table = {
+			[[\t\t line1]],
+			[[\t\t ]],
+			[[\t\t \t  line3]],
+			[[\t\t \t\t  line4]],
+		}
+		local input_string = table.concat(input_table, [[\n]])
+		local expect_table = {
+			"line1",
+			"",
+			"\t  line3",
+			"\t\t  line4",
+		}
+		local expected = table.concat(expect_table, "\n")
+
+		local result = get_dedent_result(input_string)
+
+		assert.are.same(expected, result)
+	end)
+end)
+
+describe("str.convert_indent", function()
+	-- apparently clear() needs to run before anything else...
+	ls_helpers.clear()
+	exec("set rtp+=" .. os.getenv("LUASNIP_SOURCE"))
+	local function get_convert_indent_result(input_string, indent_string)
+		local result = exec_lua(
+			string.format(
+				[[return require("luasnip.util.str").convert_indent("%s", "%s")]],
+				input_string,
+				indent_string
+			)
+		)
+		return result
+	end
+
+	it("two spaces to tab", function()
+		local input_table = {
+			"line1: no indent",
+			"",
+			"  line3: 1 indent",
+			"    line4: 2 indent",
+		}
+		local input_string = table.concat(input_table, [[\n]])
+		local indent_string = "  "
+		local expect_table = {
+			"line1: no indent",
+			"",
+			"\tline3: 1 indent",
+			"\t\tline4: 2 indent",
+		}
+		local expected = table.concat(expect_table, "\n")
+
+		local result = get_convert_indent_result(input_string, indent_string)
+
+		assert.are.same(expected, result)
+	end)
+	it([[literal \t to tab]], function()
+		local input_table = {
+			"line1: no indent",
+			"",
+			[[\\tline3: 1 indent]],
+			[[\\t\\tline4: 2 indent]],
+		}
+		local input_string = table.concat(input_table, [[\n]])
+		local indent_string = [[\\t]]
+		local expect_table = {
+			"line1: no indent",
+			"",
+			"\tline3: 1 indent",
+			"\t\tline4: 2 indent",
+		}
+		local expected = table.concat(expect_table, "\n")
+
+		local result = get_convert_indent_result(input_string, indent_string)
+
+		assert.are.same(expected, result)
+	end)
+end)
