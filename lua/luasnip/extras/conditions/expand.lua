01c1e6fde53003afdef4134dc7cc91348a470651
Michael Fortunato <michael.n.fortunato@gmail.com>
2025-01-24 17:44:22 -0600

    feat: implement `wordTrig` behavior as a condition
    >[!NOTE]
    > Maybe this pr should just be a code example in documentation or
    > even a post on a luansip community forum.
    
    The wordTrig flag will only expand the snippet if
    the proceeding character is NOT %w or `_`.
    This is quite useful. The only issue is that the characters
    on which we negate on hard coded. See here for the actual implementation
    [snippet.lua#L827](https://github.com/L3MON4D3/LuaSnip/blob/c9b9a22904c97d0eb69ccb9bab76037838326817/lua/luasnip/nodes/snippet.lua#L827)
    
    As a result, authors willl turn their plain triggers into regexTrig=true
    triggers and proceed their regex with a negated capture group.
    The issue is that the capture group on which the pattern matched, although
    its negated, still expands with the rest of the trigger.
    So people have worked around that by doing inserting the capture group
    back into the snippet. The main examples of latex + neovim users adopting this
    pattern besides me :), in current repos is in this very helpful article
    
    - A very helpful and influential article for those using neovim + LaTeX proposes this injection technique:
    <https://ejmastnak.com/tutorials/vim-latex/luasnip/#after-a>
    - Here is one such example of its usage <https://github.com/ejmastnak/dotfiles/blob/29d4e52ab0fef8fc7ff1a3e2f8b78b8dc297ff2f/config/nvim/LuaSnip/tex/math.lua#L384>
    
    ## Why the workaround is an issue
    
    This is an issue because it can break LuaSnips understanding
    of parent and child snippets, resulting in broken jump_next() etc.
    For instance, consider
    ```text
    $mbb$
       ^
    Cursor is here
    ```
    Some latex snippet authors will have their snippet definition
    for mbb look like s(trig="([^%w])mbb", t("\mathbb{}")
    The problem is that this consume the leading `$~ character, and even if
    the snippet re-inserts the `$` back, the parent snippet $$ will be broken.
    
    I think the character wordTrig=true uses should be customized
    A condition seems like the best way to do it.

--- lua/luasnip/extras/conditions/expand.lua
+++ lua/luasnip/extras/conditions/expand.lua
@@ -11,4 +11,51 @@ local function line_begin(line_to_cursor, matched_trigger)
 end
 M.line_begin = cond_obj.make_condition(line_begin)
 
+--- The wordTrig flag will only expand the snippet if
+--- the proceeding character is NOT %w or `_`.
+--- This is quite useful. The only issue is that the characters
+--- on which we negate on hard coded. See here for the actual implementation
+--- https://github.com/L3MON4D3/LuaSnip/blob/c9b9a22904c97d0eb69ccb9bab76037838326817/lua/luasnip/nodes/snippet.lua#L827
+---
+--- As a result, authors willl turn their plain triggers into regexTrig=true
+--- triggers and proceed their regex with a negated capture group.
+--- The issue is that the capture group on which the pattern matched, although
+--- its negated, still expands with the rest of the trigger.
+--- So people have worked around that by doing inserting the capture group
+--- back into the snippet
+--- https://ejmastnak.com/tutorials/vim-latex/luasnip/#after-a
+---
+--- This is an issue because it can break LuaSnips understanding
+--- of parent and child snippets, resulting in broken jump_next() etc.
+--- For instance, consider
+--- ```text
+--- $mbb$
+---    ^
+--- Cursor is here
+--- ```
+--- Some latex snippet authors will have their snippet definition
+--- for mbb look like s(trig="([^%w])mbb", t("\mathbb{}")
+--- The problem is that this consume the leading `$~ character, and even if
+--- the snippet re-inserts the `$` back, the parent snippet $$ will be broken.
+---
+--- I think the character wordTrig=true uses should be customized
+--- A condtion seems like the best way to do it
+--- @param string pattern should be a character class eg `[%w]`
+local function make_trigger_does_not_follow_char(pattern)
+	local condition = function(line_to_cursor, matched_trigger)
+		local line_to_trigger_len = #line_to_cursor - #matched_trigger
+		if line_to_trigger_len == 0 then
+			return true
+		end
+		return not string
+			.sub(line_to_cursor, line_to_trigger_len, line_to_trigger_len)
+			:match(pattern)
+	end
+	return cond_obj.make_condition(condition)
+end
+
+M.line_begin = cond_obj.make_condition(line_begin)
+M.make_trigger_does_not_proceed = make_trigger_does_not_follow_char
+M.word_trig_condition = M.make_trigger_does_not_proceed("[%w_]")
+
 return M
