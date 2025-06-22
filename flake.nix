ddcf506dabe504c323acc2d7d58c118634e4e094
L3MON4D3 <simljk@outlook.de>
2025-06-22 17:51:58 +0200

    doc: add panvimdoc and vimdoc make-step, use in ci.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A nix flake for developing LuaSnip.";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
   # has tree-sitter 0.20.8, which is required for neovim 0.9.0.
   inputs.nixpkgs-treesitter.url = "github:nixos/nixpkgs/7a339d87931bba829f68e94621536cad9132971a";
 
@@ -14,10 +16,11 @@
   inputs.luals-mdgen.url = "github:L3MON4D3/luals-mdgen";
   inputs.emmylua-analyzer-rust.url = "github:EmmyLuaLs/emmylua-analyzer-rust";
 
-  outputs = { self, nixpkgs-treesitter, nvim_07, nvim_09, nvim_master, luals-mdgen, emmylua-analyzer-rust }:
+  outputs = { self, nixpkgs, nixpkgs-treesitter, nvim_07, nvim_09, nvim_master, luals-mdgen, emmylua-analyzer-rust }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs-treesitter.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
         pkgs-treesitter = import nixpkgs-treesitter { inherit system; };
         pkgs-nvim_09 = import nvim_09.inputs.nixpkgs { inherit system; };
         pkgs-nvim_07 = import nvim_07.inputs.nixpkgs { inherit system; };
@@ -26,14 +29,14 @@
       });
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs-treesitter, pkgs-nvim_09, pkgs-nvim_07, pkg-luals-mdgen, pkg-emmylua-doc }: let
-        default_09_devshell = nvim_09.outputs.devShells.${pkgs-treesitter.system}.default;
-        true_bin = "${pkgs-treesitter.coreutils}/bin/true";
-        false_bin = "${pkgs-treesitter.coreutils}/bin/false";
+      devShells = forEachSupportedSystem ({ pkgs, pkgs-treesitter, pkgs-nvim_09, pkgs-nvim_07, pkg-luals-mdgen, pkg-emmylua-doc }: let
+        default_09_devshell = nvim_09.outputs.devShells.${pkgs.system}.default;
+        true_bin = "${pkgs.coreutils}/bin/true";
+        false_bin = "${pkgs.coreutils}/bin/false";
       in {
-        default = pkgs-treesitter.mkShell {
+        default = pkgs.mkShell {
           # use any nixpkgs here.
-          packages = with pkgs-treesitter; [
+          packages = with pkgs; [
             (aspellWithDicts (dicts: with dicts; [en]))
             gnumake
 
@@ -44,10 +47,11 @@
             gcc
             pkg-luals-mdgen
             pkg-emmylua-doc
+            panvimdoc
           ];
         };
         # clang stdenv does not build, and it's used by de.
-        test_nvim_07 = (nvim_07.outputs.devShell.${pkgs-treesitter.system}.override { stdenv = pkgs-nvim_07.gccStdenv; }).overrideAttrs(attrs: {
+        test_nvim_07 = (nvim_07.outputs.devShell.${pkgs.system}.override { stdenv = pkgs-nvim_07.gccStdenv; }).overrideAttrs(attrs: {
           TEST_07=true_bin;
           TEST_09=false_bin;
           TEST_MASTER=false_bin;
@@ -98,7 +102,7 @@
           shellHook = "";
         });
 
-        test_nvim_master = nvim_master.outputs.devShells.${pkgs-treesitter.system}.default.overrideAttrs(attrs: {
+        test_nvim_master = nvim_master.outputs.devShells.${pkgs.system}.default.overrideAttrs(attrs: {
           TEST_07=false_bin;
           TEST_09=false_bin;
           TEST_MASTER=true_bin;
