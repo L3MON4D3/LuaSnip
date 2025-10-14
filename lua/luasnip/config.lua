25ef218bd57cdf7c81b30f11590890123ab15748
L3MON4D3 <simljk@outlook.de>
2025-10-14 22:14:24 +0200

    move the jump_active-check into the autocommand.
    Whenever update_dependents is called by luasnip, we can be sure that
    it's safe to call currently.

--- lua/luasnip/config.lua
+++ lua/luasnip/config.lua
@@ -103,7 +103,14 @@ c = {
 		end
 		ls_autocmd(
 			session.config.update_events,
-			require("luasnip").active_update_dependents
+			function()
+				-- don't update due to events if an update due to luasnip is pending anyway.
+				-- (Also, this would be bad because luasnip may not be in an
+				-- consistent state whenever an autocommand is triggered)
+				if not session.jump_active then
+					require("luasnip").active_update_dependents()
+				end
+			end
 		)
 		if session.config.region_check_events ~= nil then
 			ls_autocmd(session.config.region_check_events, function()
