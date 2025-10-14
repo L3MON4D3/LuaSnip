21e9fecfc07fb2cd707b6c7c3fa148550a34d053
L3MON4D3 <simljk@outlook.de>
2025-10-14 17:33:28 +0200

    fix: cache result of vim.version() (fix #1393).
    The linked issue revealed that `vim.version()` dominates the runtime of
    `match_snippet`, which, if autosnippets are enabled, is called on every
    keystroke.

--- lua/luasnip/util/vimversion.lua
+++ lua/luasnip/util/vimversion.lua
@@ -1,7 +1,7 @@
 local M = {}
 
+local version = vim.version()
 function M.ge(maj, min, patch)
-	local version = vim.version()
 	return
 		(version.major > maj)
 			or (version.major == maj and version.minor > min)
