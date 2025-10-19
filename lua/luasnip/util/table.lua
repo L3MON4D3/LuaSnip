ab816819d9dfd5655f16fc7370178150a87280f5
L3MON4D3 <simljk@outlook.de>
2025-10-19 11:58:51 +0200

    improve logging of dynamicNode.

--- lua/luasnip/util/table.lua
+++ lua/luasnip/util/table.lua
@@ -33,7 +33,13 @@ local function list_to_set(values)
 	return list
 end
 
+-- http://lua-users.org/wiki/VarargTheSecondClassCitizen
+local function pack2(...) return {n=select('#', ...), ...} end
+local function unpack2(t) return unpack(t, 1, t.n) end
+
 return {
 	list_to_set = list_to_set,
 	set_to_list = set_to_list,
+	pack = pack2,
+	unpack = unpack2
 }
